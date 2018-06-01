fe8792534f00e64ed6ea864c02f477d312bdb767
wjdrudtn0225 <wjdrudtn0225@naver.com>
2018-06-01 19:05:11 +0900

    second commit

--- movieapp/src/Movie/Movie.js
+++ movieapp/src/Movie/Movie.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css';
 
+
 class Movie extends Component{
+
+  static propTypes = {
+    title:  PropTypes.string.isRequired,
+    poster: PropTypes.string
+  }
+
   render(){
     return(
       <div>
